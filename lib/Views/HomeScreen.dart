56a52631c5e9cf6e0873712775c74c55daa0931e
Admin <maayush235@gmail.com>
2024-08-01 22:19:54 +0530

    Radhey Radhey

--- lib/Views/HomeScreen.dart
+++ lib/Views/HomeScreen.dart
@@ -1,11 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
-
 import '../Controller/data_controller.dart';
 
-
 class HomeScreen extends StatelessWidget {
-   HomeScreen({super.key});
+  HomeScreen({super.key});
 
   final DataController dataController = Get.put(DataController());
 
@@ -21,18 +19,78 @@ class HomeScreen extends StatelessWidget {
         } else if (dataController.quotes.isEmpty) {
           return const Center(child: Text('No data available'));
         } else {
-          return ListView.builder(
+          return PageView.builder(
             itemCount: dataController.quotes.length,
             itemBuilder: (context, index) {
               final quote = dataController.quotes[index];
-              return ListTile(
-                title: Text(quote.quote),
-                subtitle: Text('${quote.author} - ${quote.category}'),
+              return Stack(
+                fit: StackFit.expand,
+                children: [
+                  // Background Image
+                  Image.asset(
+                    getImageForCategory(quote.category),
+                    fit: BoxFit.cover,
+                  ),
+                  // Quote Text
+                  Container(
+                    color: Colors.black54, // Add a slight background for readability
+                    padding: const EdgeInsets.all(16.0),
+                    child: Center(
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Text(
+                            quote.quote,
+                            style: TextStyle(
+                              color: Colors.white,
+                              fontSize: 24.0,
+                              fontStyle: FontStyle.italic,
+                            ),
+                            textAlign: TextAlign.center,
+                          ),
+                          SizedBox(height: 16.0),
+                          Text(
+                            '- ${quote.author}',
+                            style: TextStyle(
+                              color: Colors.white70,
+                              fontSize: 18.0,
+                            ),
+                            textAlign: TextAlign.center,
+                          ),
+                          SizedBox(height: 8.0),
+                          Text(
+                            quote.category,
+                            style: TextStyle(
+                              color: Colors.white54,
+                              fontSize: 16.0,
+                            ),
+                            textAlign: TextAlign.center,
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                ],
               );
             },
           );
         }
       }),
     );
+  }
+
+  // Function to get the image based on the category
+  String getImageForCategory(String category) {
+    switch (category.toLowerCase()) {
+      case 'inspiration':
+        return 'assets/image2.jpeg';
+      case 'love':
+        return 'assets/image9.jpeg';
+      case 'life':
+        return 'assets/life.jpeg';
+    // Add more categories and images as needed
+      default:
+        return 'assets/default.jpeg';
+    }
   }
 }
