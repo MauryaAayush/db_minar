93c5800c6f3108d352d92ed6870653bbd8130a02
Admin <maayush235@gmail.com>
2024-08-04 15:47:17 +0530

    Radhey Radhey

--- lib/Views/quotes_screen.dart
+++ lib/Views/quotes_screen.dart
@@ -1,7 +1,9 @@
+import 'package:db_minar/Views/theme_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import '../Controller/data_controller.dart';
 import '../Model/quotes_model.dart';
+import 'Topic_Screen.dart';
 
 class QuotesScreen extends StatefulWidget {
   final String category;
@@ -36,89 +38,134 @@ class _QuotesScreenState extends State<QuotesScreen> {
               ),
             ),
           ),
-          SingleChildScrollView(
-            child: Padding(
-              padding: const EdgeInsets.all(15.0),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Stack(
+          Obx(() {
+            if (dataController.isLoading.value) {
+              return Center(child: CircularProgressIndicator());
+            } else if (dataController.quotes.isEmpty) {
+              return Center(
+                child: Text(
+                  'No quotes found for this category',
+                  style: TextStyle(fontSize: 18, color: Colors.grey),
+                ),
+              );
+            } else {
+              return PageView.builder(
+                scrollDirection: Axis.vertical,
+                itemCount: dataController.quotes.length,
+                itemBuilder: (context, index) {
+                  var quote = dataController.quotes[index];
+                  return Stack(
                     children: [
-                      Positioned(
-                        top: 35,
-                        child: IconButton(
-                          icon: const Icon(Icons.arrow_back, color: Colors.white),
-                          onPressed: () {
-                            Navigator.of(context).pop();
-                          },
-                        ),
-                      ),
-                      Center(
-                        child: Padding(
-                          padding: const EdgeInsets.only(top: 40.0),
-                          child: Text(
-                            'Quotes',
-                            style: TextStyle(
-                              fontSize: 24,
-                              fontWeight: FontWeight.bold,
-                              color: Colors.white,
-                            ),
+                      Container(
+                        color: Colors.black54, // Add a slight background for readability
+                        padding: const EdgeInsets.all(16.0),
+                        child: Center(
+                          child: Column(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              Text(
+                                quote.quote,
+                                style: const TextStyle(
+                                  color: Colors.white,
+                                  fontSize: 24.0,
+                                  fontStyle: FontStyle.italic,
+                                ),
+                                textAlign: TextAlign.center,
+                              ),
+                              const SizedBox(height: 16.0),
+                              Text(
+                                '- ${quote.author}',
+                                style: const TextStyle(
+                                  color: Colors.white70,
+                                  fontSize: 18.0,
+                                ),
+                                textAlign: TextAlign.center,
+                              ),
+                              const SizedBox(height: 8.0),
+                            ],
                           ),
                         ),
                       ),
-                    ],
-                  ),
-                  const SizedBox(height: 30),
-                  Obx(() {
-                    if (dataController.isLoading.value) {
-                      return Center(child: CircularProgressIndicator());
-                    } else if (dataController.quotes.isEmpty) {
-                      return Center(
+                      Positioned(
+                          top: 40,
+                          child: IconButton(onPressed: () {
+                        Get.back();
+                      }, icon: Icon(Icons.arrow_back,color: Colors.white,))),
+                      Positioned(
+                        top: 50,
+                        left: 145,
                         child: Text(
-                          'No quotes found for this category',
-                          style: TextStyle(fontSize: 18, color: Colors.grey),
+                          quote.category,
+                          style: const TextStyle(
+                            color: Colors.white,
+                            fontSize: 23.0,
+                            fontWeight: FontWeight.bold,
+                          ),
                         ),
-                      );
-                    } else {
-                      return ListView.builder(
-                        itemCount: dataController.quotes.length,
-                        shrinkWrap: true,
-                        itemBuilder: (context, index) {
-                          var quote = dataController.quotes[index];
-                          return Card(
-                            elevation: 2,
-                            shape: RoundedRectangleBorder(
-                              borderRadius: BorderRadius.circular(10),
-                            ),
-                            margin: const EdgeInsets.symmetric(vertical: 8.0, horizontal: 16.0),
-                            child: ListTile(
-                              title: Text(
-                                quote.quote,
-                                style: TextStyle(fontSize: 16),
-                              ),
-                              subtitle: Text('- ${quote.author}'),
-                              trailing: IconButton(
-                                icon: Icon(
-                                  Icons.favorite,
-                                  color: dataController.likedQuotes.contains(quote)
-                                      ? Colors.red
-                                      : Colors.grey,
-                                ),
-                                onPressed: () {
-                                  dataController.toggleLike(quote, index);
-                                },
-                              ),
-                            ),
-                          );
-                        },
-                      );
-                    }
-                  }),
-                ],
-              ),
+                      ),
+                      Positioned(
+                        top: 50,
+                        right: 16.0,
+                        child: IconButton(
+                          icon: Icon(
+                            dataController.quotes[index].isLiked == "1" ? Icons.favorite : Icons.favorite_border,
+                            color: dataController.quotes[index].isLiked == "1" ? Colors.red : Colors.white,
+                          ),
+
+
+                          onPressed: () {
+                            dataController.toggleLike(quote,index);
+                          },
+                        ),
+                      ),
+                    ],
+                  );
+                },
+              );
+            }
+          }),
+          Positioned(
+            bottom: 16.0,
+            left: 0,
+            right: 0,
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+
+                TransparentButton(
+                  text: 'Theme',
+                  icon: Icons.palette,
+                  onPressed: () {
+                    Get.to(() => ThemeScreen());
+                  },
+                ),
+
+              ],
             ),
           ),
         ],
+      ),
+    );
+  }
+}
+
+
+class TransparentButton extends StatelessWidget {
+  final String text;
+  final IconData icon;
+  final VoidCallback onPressed;
+
+  const TransparentButton({super.key, required this.text, required this.icon, required this.onPressed});
+
+  @override
+  Widget build(BuildContext context) {
+    return TextButton.icon(
+      onPressed: onPressed,
+      icon: Icon(icon, color: Colors.white),
+      label: Text(text),
+      style: TextButton.styleFrom(
+        foregroundColor: Colors.white,
+        backgroundColor: Colors.black.withOpacity(0.2), // Transparent background
       ),
     );
   }
