5ffaec0a790cf3bc4143017ab335afe853de5a6c
Admin <maayush235@gmail.com>
2024-08-05 12:17:24 +0530

    Radhey Radhey

--- lib/Views/Topic_Screen.dart
+++ lib/Views/Topic_Screen.dart
@@ -3,52 +3,12 @@ import 'package:db_minar/Views/quotes_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import '../Model/quotes_model.dart';
+import '../utils/lists.dart';
 
 class TopicScreen extends StatelessWidget {
   TopicScreen({super.key});
 
-  final List<Topic> topics = [
-    Topic(
-        heading: 'Motivation',
-        category: 'Motivation',
-        image: 'assets/self_discipline.jpeg'),
-    Topic(
-        heading: 'Motivation',
-        category: 'Love',
-        image: 'assets/happiness.jpeg'),
-    Topic(
-        heading: 'Motivation',
-        category: 'Positive',
-        image: 'assets/positivity.jpeg'),
-    Topic(
-        heading: 'Inspiration',
-        category: 'Success',
-        image: 'assets/success.jpeg'),
-    Topic(
-        heading: 'Inspiration',
-        category: 'Happy',
-        image: 'assets/fitness.jpeg'),
-    Topic(
-        heading: 'Inspiration',
-        category: 'Kindness',
-        image: 'assets/freedom.jpeg'),
-    Topic(
-        heading: 'Inspiration',
-        category: 'Loyalty',
-        image: 'assets/creativity.jpeg'),
-    Topic(
-        heading: 'Hard Times',
-        category: 'Sad',
-        image: 'assets/sadness.jpeg'),
-    Topic(
-        heading: 'Hard Times',
-        category: 'Ego',
-        image: 'assets/depression.jpeg'),
-    Topic(
-        heading: 'Hard Times',
-        category: 'Gratitude',
-        image: 'assets/broken.jpeg'),
-  ];
+
 
   @override
   Widget build(BuildContext context) {
---
+++ lib/utils/lists.dart
@@ -0,0 +1,45 @@
+
+import '../Model/quotes_model.dart';
+
+final List<Topic> topics = [
+  Topic(
+      heading: 'Motivation',
+      category: 'Motivation',
+      image: 'assets/self_discipline.jpeg'),
+  Topic(
+      heading: 'Motivation',
+      category: 'Love',
+      image: 'assets/happiness.jpeg'),
+  Topic(
+      heading: 'Motivation',
+      category: 'Positive',
+      image: 'assets/positivity.jpeg'),
+  Topic(
+      heading: 'Inspiration',
+      category: 'Success',
+      image: 'assets/success.jpeg'),
+  Topic(
+      heading: 'Inspiration',
+      category: 'Happy',
+      image: 'assets/fitness.jpeg'),
+  Topic(
+      heading: 'Inspiration',
+      category: 'Kindness',
+      image: 'assets/freedom.jpeg'),
+  Topic(
+      heading: 'Inspiration',
+      category: 'Loyalty',
+      image: 'assets/creativity.jpeg'),
+  Topic(
+      heading: 'Hard Times',
+      category: 'Sad',
+      image: 'assets/sadness.jpeg'),
+  Topic(
+      heading: 'Hard Times',
+      category: 'Ego',
+      image: 'assets/depression.jpeg'),
+  Topic(
+      heading: 'Hard Times',
+      category: 'Gratitude',
+      image: 'assets/broken.jpeg'),
+];
